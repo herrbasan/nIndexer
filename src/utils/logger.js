b856e92424b06cc6bd01d15fd689b8155201841c
herrbasan <david@raum.com>
2026-04-20 16:15:50 +0200

    fix(logger): proxy nLogger to console for service monitoring

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -14,7 +14,18 @@ const loggerInstance = createLogger(loggerOptions);
 
 // Export the instance as default getLogger
 export function getLogger() {
-    return loggerInstance;
+    return new Proxy(loggerInstance, {
+        get(target, prop) {
+            if (['info', 'warn', 'error', 'debug'].includes(prop)) {
+                return (msg, meta, type, opts = {}) => {
+                    const method = prop === 'error' ? console.error : console.log;
+                    method(`[${String(type || 'System').padEnd(10)}] [${prop.toUpperCase().padEnd(5)}] ${msg}`, meta ? Object.keys(meta).length ? meta : '' : '');
+                    return target[prop](msg, meta, type, opts);
+                };
+            }
+            return target[prop];
+        }
+    });
 }
 
 export { createLogger, resetLogger };
